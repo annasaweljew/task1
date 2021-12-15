acac7a1e174a6afb98bbdf2107a63aacbebd655d
Caxora <caxora@protonmail.com>
2021-12-15 17:16:21 +0100

    add task description to README file

--- src/test/java/org/example/task/TaskTest.java
+++ src/test/java/org/example/task/TaskTest.java
@@ -3,6 +3,9 @@ package org.example.task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TaskTest {
@@ -32,7 +35,7 @@ public class TaskTest {
     public void should_sum_two_decimal_values() {
         double a = 27.5487746;
         double b = 94.84745641;
-        assertEquals(122.3962311, task.sum(a,b));
+        assertEquals(Math.round(122.40) , task.sum(a,b));
     }
 
     @Test
