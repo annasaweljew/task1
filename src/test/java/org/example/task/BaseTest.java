58e89f9aad274cb2fcb0a3506be9f6aafae4f9f8
Anna Saweljew <anna.saweljew@o2.pl>
2021-12-19 15:20:36 +0100

    log test results to txt file

--- src/test/java/org/example/task/BaseTest.java
+++
@@ -1,7 +0,0 @@
-package org.example.task;
-
-public class BaseTest {
-
-    //run test and save result to txt file
-
-}
--- src/test/java/org/example/task/TaskTest.java
+++ src/test/java/org/example/task/TaskTest.java
@@ -1,36 +1,54 @@
 package org.example.task;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TaskTest {
+
+    private final TestLogger logger = new TestLogger();
 
     @Test
     void should_sum_two_positive_values() {
         double a = 47;
         double b = 53;
-        assertEquals(100, Task.sum(a,b));
+        var result = Task.sum(a,b);
+        logger.logSingleTestResult("should_sum_two_positive_values", result==100);
+        assertEquals(100, result);
     }
 
     @Test
     void should_sum_two_negative_values() {
         double a = -47;
         double b = -53;
-        assertEquals(-100, Task.sum(a,b));
+        var result = Task.sum(a,b);
+        logger.logSingleTestResult("should_sum_two_negative_values", result==-100);
+        assertEquals(-100, result);
     }
 
     @Test
     void should_sum_two_decimal_values() {
         double a = 27.5487746;
         double b = 94.84745641;
-        assertEquals(Math.round(122.40) , Task.sum(a,b));
+        var result = Task.sum(a,b);
+        logger.logSingleTestResult("should_sum_two_decimal_values", result==Math.round(122.40));
+        assertEquals(Math.round(122.40) , result);
     }
 
     @Test
     void should_sum_two_zero_values() {
         double a = 0;
         double b = 0;
-        assertEquals(0, Task.sum(a,b));
+        var result = Task.sum(a,b);
+        logger.logSingleTestResult("should_sum_two_zero_values", result==0);
+        assertEquals(0, result);
+    }
+
+    @AfterAll
+    void logTestResults() {
+        logger.saveResults();
     }
 }
